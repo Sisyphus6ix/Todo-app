c1f326de2179dcf22510d25fb7bee61f29cef7f0
Ryan Silvera <ryan.silvera@icloud.com>
2023-01-31 22:40:11 -0500

    Cleaning up code

--- src/index.js
+++ src/index.js
@@ -1,34 +1,14 @@
-import { allProjects } from "./project"
-import { project } from "./project"
+import { projectModal, submitProject } from "./project"
 
 let projectBtn = document.getElementById('projectBtn')
-let modal = document.querySelector('#projectModal')
 let projectSubmitBtn = document.getElementById('submitBtn')
-let projectDivContainer = document.getElementsByClassName('projectDiv')
 
-
-
-const test = () => {
-    alert('This button works')
-}
-
-// Makes modal appear
+// Makes project modal appear
 projectBtn.addEventListener('click', () => {
-    modal.showModal()
+    projectModal()
 })
 
-//Submit button in modal functionality
+//Project modal submit button
 projectSubmitBtn.addEventListener('click', () => {
-    // Storing the value of the input button in my array
-    let projectName = document.getElementById('projectName')
-    let storedProjectName = new project(projectName.value)
-    allProjects.push(storedProjectName.title)
-
-    //Updating my html with the projects from the array
-    let projectDiv = projectDivContainer[0].appendChild(document.createElement('div'))
-    projectDiv.innerText = storedProjectName.title
-    projectDiv.setAttribute('class', 'projects')
-
-    //Clearing the input
-    projectName.value = ''
+   submitProject()
 })
--- src/project.js
+++ src/project.js
@@ -4,5 +4,25 @@ function project (title) {
     this.title = title;
 }
 
-export { allProjects }
-export { project }
+const projectModal = () => {
+    let modal = document.querySelector('#projectModal')
+    modal.showModal()
+}
+
+const submitProject = () => {
+    // Storing the inputs value into array
+    let projectName = document.getElementById('projectName')
+    let storedProjectName = new project(projectName.value)
+    allProjects.push(storedProjectName.title)
+
+    //Updating my html with the projects from the array
+    let projectDivContainer = document.getElementsByClassName('projectDiv')
+    let projectDiv = projectDivContainer[0].appendChild(document.createElement('div'))
+    projectDiv.innerText = storedProjectName.title
+    projectDiv.setAttribute('class', 'projects')
+
+    //Clearing the input
+    projectName.value = ''
+}
+
+export { projectModal, submitProject }
