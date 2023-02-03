ff132465e62cbdbe773d2dd05f6071db6b3d75c5
Ryan Silvera <ryan.silvera@icloud.com>
2023-02-02 19:03:49 -0500

    Work on logic

--- src/task.js
+++ src/task.js
@@ -2,21 +2,26 @@ import { allProjects } from "./project";
 
 const test = () => {
     let editProjects = document.querySelectorAll('projectDiv')
+    let projectTitleContainer = document.getElementsByClassName('rightside-header')
+    let projectTitle = projectTitleContainer[0]
     console.log(editProjects)
 
     // Working
-    // for (let project of projects) {
-    //     project.addEventListener('click', function(event) {
-    //         console.log(event.target.innerText)
-    //     })
-    // }
+    for (let project of projects) {
+        project.addEventListener('click', function(event) {
+            console.log(event.target.innerText)
+            projectTitle.innerText = ''
+            projectTitle.innerText = event.target.innerText
+        })
+    }
 
     // Not Working
-    editProjects.addEventListener('click', (event) => {
-        if(event.target.classList.contains('project')){
-            console.log(e.target.innerText)
-        }
-    })
+    // editProjects.addEventListener('click', (event) => {
+    //     if(event.target.classList.contains('project')){
+    //         console.log(e.target.innerText)
+    //     }
+    // })
+
 
 }
 
