c4b0175040ea43645bfecea2f5bc0bda5c5b7af2
Ryan Silvera <ryan.silvera@icloud.com>
2023-02-17 22:48:32 -0500

    Add task constructor

--- src/task.js
+++ src/task.js
@@ -1,5 +1,11 @@
 import { allProjects } from "./project";
 
+// constructor for new tasks
+function task(title, description) {
+    this.title = title
+    this.description = description
+}
+
 export const taskModal = () => {
     let modal = document.querySelector('#taskModal')
     modal.showModal()
@@ -7,11 +13,16 @@ export const taskModal = () => {
 
 export const submitTask = () => {
     let taskName = document.getElementById('taskName')
-    let storedTaskName = taskName.value
-    console.log(storedTaskName)
+    let taskDescription = document.getElementById('taskDescription')
+    let taskInfo = new task(taskName.value, taskDescription.value)
+    console.log(taskInfo)
 
-    // for (let i = 0; i < allProjects.length; i++){
-    //     const project = allProjects[i]
-    //     console.log(project)
-    // }
+    // console.log(document.getElementsByClassName('active'))
+
+    for (let i = 0; i < allProjects.length; i++){
+        const project = allProjects[i]
+        console.log(project)
+    }
+
+    // console.log(allProjects)
 }
